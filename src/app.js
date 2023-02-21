19224c941a279233280db305a36f462d67642cf5
Jminx42 <105382626+Jminx42@users.noreply.github.com>
2023-02-21 09:42:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseParagraph = document.getElementById("excuse");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let randomNumWho = Math.floor(Math.random() * who.length);
+  let randomNumAction = Math.floor(Math.random() * action.length);
+  let randomNumWhat = Math.floor(Math.random() * what.length);
+  let randomNumWhen = Math.floor(Math.random() * when.length);
+
+  excuseParagraph.innerText =
+    who[randomNumWho] +
+    " " +
+    action[randomNumAction] +
+    " " +
+    what[randomNumWhat] +
+    " " +
+    when[randomNumWhen];
 };
